2c829a38542e0d5d39f495b217ab41389e74bb1f
balaji2210 <balajiatkuri10@gmail.com>
2023-09-08 18:39:49 +0530

    fix image width and height

--- components/card/Card.js
+++ components/card/Card.js
@@ -1,7 +1,13 @@
 const Card = ({ title = "", description = "", image = "" }) => {
   return (
     <div className="card" style={{ width: " 18rem", cursor: "pointer" }}>
-      <img src={image} className="card-img-top" alt="image" />
+      <img
+        src={image}
+        width="200px"
+        height="200px"
+        className="card-img-top"
+        alt="image"
+      />
       <div className="card-body">
         <p className="card-text fs-5 ">{title?.toUpperCase()}</p>
         <p className="card-text">{description}</p>
